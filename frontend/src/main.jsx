744aef570c1d578519fd9499f0b060c94018dcb2
technicalguftgu <meghanar19@gmail.com>
2025-03-08 21:44:05 +0000

    added files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client'; // new method in React 18
+import './index.css'; // global styles (CSS file)
+import App from './App.jsx'; // main App component
 
+// Create a root element and render the app to the DOM
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <StrictMode> {/* Enables extra checks and warnings during development */}
+    <App />  {/* The root component */}
+  </StrictMode>
+);
